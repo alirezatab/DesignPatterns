436f3478bedf515cc635394ddaf11206e9e8366f
alirezatab <alirezatabrizi10@gmail.com>
2016-11-10 13:35:49 -0800

    added the HTTP-API, outlets and a navagationController.

--- DesignPatterns/ViewController.swift
+++ DesignPatterns/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var dataTable: UITableView!
+    @IBOutlet var toolbar: UIToolbar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
