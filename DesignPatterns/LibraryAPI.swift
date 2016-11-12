a1ca40540653b0350ba34d5a05016283ec4a48e6
alirezatab <alirezatabrizi10@gmail.com>
2016-11-11 19:45:35 -0800

    added Facade design patter

--- DesignPatterns/LibraryAPI.swift
+++ DesignPatterns/LibraryAPI.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class LibraryAPI: NSObject {
+    
+    ///Facade var below
+    private let persistencyManager : PersistencyManager
+    private let httpClient : HTTPClient
+    private let isOnline : Bool
+    
     //1
     class var sharedInstance : LibraryAPI{
         //2
@@ -18,6 +24,36 @@ class LibraryAPI: NSObject {
         }
         //4
         return singelton.instance
+    }
+    
+    ///Facade init
+    // isOnline determines if the server should be updated with any changes made to the albums list, such as added or deleted albums.
+    //The HTTP client doesn’t actually work with a real server and is only here to demonstrate the usage of the facade pattern, so isOnline will always be false.
+    override init(){
+        persistencyManager = PersistencyManager()
+        httpClient = HTTPClient()
+        isOnline = false
+        
+        super.init()
+    }
+    
+    func getAlbums() -> [Album] {
+        return persistencyManager.getAlbums()
+    }
+    
+    //Take a look at addAlbum(_:index:). The class first updates the data locally, and then if there’s an internet connection, it updates the remote server. This is the real strength of the Facade;
+    func addAlbum(album: Album, index:Int) {
+        persistencyManager.addAlbum(album: album, index: index)
+        if isOnline {
+            httpClient.postRequest("/api/addAlbum", body: album.description)
+        }
+    }
+    
+    func deleteAlbum(index: Int) {
+        persistencyManager.deleteAlbumAtIndex(index: index)
+        if isOnline {
+            httpClient.postRequest("/api/deleteAlbum", body: "\(index)")
+        }
     }
 }
 
