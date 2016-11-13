47e318665fda151fe3d750e8f1daff3663553d5d
alirezatab <alirezatabrizi10@gmail.com>
2016-11-12 22:35:51 -0800

    Decorator Design Patter- Extensions

---
+++ DesignPatterns/AlbumExtensions.swift
@@ -0,0 +1,13 @@
+//
+//  AlbumExtensions.swift
+//  DesignPatterns
+//
+//  Created by ALIREZA TABRIZI on 11/12/16.
+//  Copyright © 2016 AR-T.com, Inc. All rights reserved.
+//
+
+///Note: Classes can of course override a superclass’s method, but with extensions you can’t. Methods or properties in an extension cannot have the same name as methods or properties in the original class.
+
+import Foundation
+
+
