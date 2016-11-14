d9648dca0cdca129ec20ccb0f9a5056e4dfde84d
alirezatab <alirezatabrizi10@gmail.com>
2016-11-13 23:53:40 -0800

    Added next set of steps to finalize the HorizontalScroller view

--- DesignPatterns/HorizontalScroller.swift
+++ DesignPatterns/HorizontalScroller.swift
@@ -23,14 +23,43 @@ import UIKit
 
 class HorizontalScroller: UIView {
 
+    //This is necessary in order to prevent a retain cycle. If a class keeps a strong reference to its delegate and the delegate keeps a strong reference back to the conforming class, your app will leak memory since neither class will release the memory allocated to the other. All properties in swift are strong by default!
+    weak var delegate : HorizontalScrollerDelegate
     
+    //1
+    //Define constants to make it easy to modify the layout at design time. The view’s dimensions inside the scroller will be 100 x 100 with a 10 point margin from its enclosing rectangle.
+    private let VIEW_PADDING = 10
+    private let VIEW_DIMENTSIONS = 100
+    private let VIEWS_OFFSET = 100
     
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    //2
+    //Create the scroll view containing the views.
+    private var scroller : UIScrollView!
+    
+    //3
+    //Create an array that holds all the album covers.
+    var viewArray = [UIView]()
+    
+    override init(frame :CGRect){
+        super.init(frame: frame)
+        initializeScrollView()
     }
-    */
-
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initializeScrollView()
+    }
+    
+    func initializeScrollView() {
+        //1
+        //Create’s a new UIScrollView instance and add it to the parent view.
+        
+        //2
+        //Turn off autoresizing masks. This is so you can apply your own constraints
+        
+        //3
+        //Apply constraints to the scrollview. You want the scroll view to completely fill the HorizontalScroller
+        
+        //4
+        //Create a tap gesture recognizer. The tap gesture recognizer detects touches on the scroll view and checks if an album cover has been tapped. If so, it will notify the HorizontalScroller delegate.
+    }
 }
