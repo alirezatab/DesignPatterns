563a4e93f25ddb9d45550d5e1f0d21d254cba8dd
alirezatab <alirezatabrizi10@gmail.com>
2016-11-13 23:31:02 -0800

    Added HorizontalScroller Swift file in the view folder and about to implement the protocol

---
+++ DesignPatterns/HorizontalScroller.swift
@@ -0,0 +1,27 @@
+//
+//  HorizontalScroller.swift
+//  DesignPatterns
+//
+//  Created by ALIREZA TABRIZI on 11/13/16.
+//  Copyright © 2016 AR-T.com, Inc. All rights reserved.
+//
+
+///An Adapter allows classes with incompatible interfaces to work together. It wraps itself around an object and exposes a standard interface to interact with that object.
+
+import UIKit
+
+@objc protocol HorizontalScrollerDelegate{
+    
+}
+
+class HorizontalScroller: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
