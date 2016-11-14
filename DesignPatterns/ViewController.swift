0a1667737a238840ce6ad3a793bcd699e2236cf5
alirezatab <alirezatabrizi10@gmail.com>
2016-11-13 23:00:56 -0800

    Using Facade to represent data within the tableview. datasource and delegate just set in code... adding shoDataForAlbum Next

--- DesignPatterns/ViewController.swift
+++ DesignPatterns/ViewController.swift
@@ -8,14 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var dataTable: UITableView!
     @IBOutlet weak var toolbar: UIToolbar!
     
+    private var allAlbums = [Album]()
+    private var currentAlbumData : (titles:[String], values:[String])?
+    private var currentAlbumIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        //1
+        self.navigationController?.navigationBar.isTranslucent = false
+        currentAlbumIndex = 0
+        
+        //2
+        //Get a list of all the albums via the API. Remember, the plan is to use the facade of LibraryAPI rather than PersistencyManager directly
+        allAlbums = LibraryAPI.sharedInstance.getAlbums()
+        
+        //3
+        //the uitableview that presents the album data
+        dataTable.delegate = self
+        dataTable.dataSource = self
+        dataTable.backgroundView = nil
+        view.addSubview(dataTable!)
+    }
+    
+    func showDataForAlbum(albumIndex: Int) {
+        // defensive code: make sure the requested index is lower than the amount of albums
+        
     }
 
     override func didReceiveMemoryWarning() {
