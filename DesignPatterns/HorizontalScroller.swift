e7cd5bd50f6f101d708d16732acc72fc2a25a86d
alirezatab <alirezatabrizi10@gmail.com>
2016-11-13 23:42:48 -0800

    added the required and optional delegate for HorizontalScroller view

--- DesignPatterns/HorizontalScroller.swift
+++ DesignPatterns/HorizontalScroller.swift
@@ -11,11 +11,20 @@
 import UIKit
 
 @objc protocol HorizontalScrollerDelegate{
-    
+    // ask the delegate how many views he wants to present inside the horizontal scroller
+    func numberOfViewsForHorizontalScroller(scroller: HorizontalScroller) -> Int
+    // ask the delegate to return the view that should appear at <index>
+    func horizontalScrollerViewAtIndex(scroller: HorizontalScroller, index: Int) -> UIView
+    // inform the delegate what the view at <index> has been clicked
+    func horizontalScrollerClicledViewAtIndex(scroller: HorizontalScroller, index: Int)
+    // ask the delegate for index of the initial view to display. this method is optional and defualts to 0 if it's not implemented by the delegate
+    @objc optional func initialViewIndex(scroller: HorizontalScroller) -> Int
 }
 
 class HorizontalScroller: UIView {
 
+    
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
